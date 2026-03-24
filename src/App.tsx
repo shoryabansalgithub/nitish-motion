5ef33a7f71b450ef91060de9072426314bf72daa
Shorya <bansalshorya13@gmail.com>
2026-03-24 10:14:33 +0530

    feat: Introduce DiscoverButton and related components
    Replace AiToolbar with ConfigurationPanel and rearrange other widgets to improve the layout and showcase the new DiscoverButton component. This change also integrates several other new components like FloatingMenu, FrequencySelector, DiscreteTabs, NestedMenu, SidebarMenu, SyncButton, SettingsSheet, and DeleteButton into the main App layout.

--- src/App.tsx
+++ src/App.tsx
@@ -24,39 +24,27 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center py-20 gap-16">
       <div className="w-full flex justify-center py-8">
-        <AiToolbar />
+        <ConfigurationPanel />
+      </div>
+
+      <div className="w-full flex justify-center h-[350px] items-end pb-4">
+        <FloatingMenu />
+      </div>
+
+      <div className="w-full flex justify-center h-[120px]">
+        <FrequencySelector />
+      </div>
+
+      <div className="w-full flex justify-center">
+        <DiscreteTabs />
+      </div>
+
+      <div className="w-[480px] h-[80px] flex justify-center">
+        <DiscoverButton />
       </div>
 
       <div className="w-full flex justify-center py-8">
-        <InvitesWidget />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <SyncStatus />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <CopyCode />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <LoadingProgress />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <DeleteButton />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <SettingsSheet />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <ProjectSelector />
-      </div>
-
-      <div className="w-full flex justify-center py-8">
-        <SyncButton />
+        <NestedMenu />
       </div>
 
       <div className="w-full flex justify-center py-8">
@@ -64,27 +52,39 @@ export default function App() {
       </div>
 
       <div className="w-full flex justify-center py-8">
-        <NestedMenu />
-      </div>
-
-      <div className="w-[480px] h-[80px] flex justify-center">
-        <DiscoverButton />
-      </div>
-      
-      <div className="w-full flex justify-center">
-        <DiscreteTabs />
-      </div>
-
-      <div className="w-full flex justify-center h-[120px]">
-        <FrequencySelector />
-      </div>
-
-      <div className="w-full flex justify-center h-[350px] items-end pb-4">
-        <FloatingMenu />
+        <SyncButton />
       </div>
 
       <div className="w-full flex justify-center py-8">
-        <ConfigurationPanel />
+        <ProjectSelector />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <SettingsSheet />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <DeleteButton />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <LoadingProgress />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <CopyCode />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <SyncStatus />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <InvitesWidget />
+      </div>
+
+      <div className="w-full flex justify-center py-8">
+        <AiToolbar />
       </div>
     </div>
   );
